cfd12c09e3c9ec176549999ab5ac71791c9f21a6
Simonse S.E. (Sytze) <Sytze.Simonse@vanoord.com>
2023-01-19 10:21:01 +0100

    Aggregate point values by averaging and adding to DF with tile information

--- src/main.R
+++ src/main.R
@@ -13,7 +13,7 @@ source("src/split.R")
 # Add empty (NA) columns for trap data to DataFrame
 ## Create column names: 29 diversity indices * X no. of statistics
 diversity_indices <- colnames(trap_data_ter_spdf@data)
-statistics <- c("mean", "min", "max")
+statistics <- c("mean")
 diversity_ind_stats <- outer(diversity_indices, statistics, sep = "_", paste)
 
 ## Add columns with empty values
@@ -32,17 +32,33 @@ result_list <- lapply(1:nrow(tile_df), function(i) {
   
   # Check if the points are within the RasterLayer extent
   ## Convert SPDF to SpatialPoints
-  trap_data_ter_sp <- SpatialPoints(trap_data_ter_spdf)
+  trap_data_ter_sp <- SpatialPoints(trap_data_ter_spdf, proj4string = crs)
   
   ## Extract points
   extracted_points <- intersect(trap_data_ter_sp, tile)
+  crs(extracted_points) <- CRS("+init=epsg:32626")
+  
+  ## Check if there is more than 0 points within raster
   if (!(length(extracted_points)) == 0) {
-    print(extracted_points)
-    print(as.data.frame(extracted_points))
+    ## Select data by points
+    result <- over(extracted_points, trap_data_ter_spdf)
+    
+    # Calculate mean for each column
+    return(round(colMeans(result), 1))
   }
 })
 
-# Plot raster and overlay points
-plot(r)
-plot(trap_data_ter_sp, add = TRUE, col = "purple", pch = 16, cex = 0.8)
+# Add results of aggregating points to the tile DataFrame
+for (i in 1:nrow(tile_df)) {
+  # Check if no values were found
+  if (!(is.null(result_list[[i]]))) {
+    tile_df[i,10:38] <- result_list[i][[1]]
+  }
+}
 
+# Remove empty rows from DataFrame
+tile_df <- na.omit(tile_df)
+
+# Plot raster and overlay points
+# plot(r)
+# plot(trap_data_ter_sp, add = TRUE, col = "purple", pch = 16, cex = 0.8)
